5dc1ca8f8e479ee5c03d32c7bce4f09adf908343
Jishnu <jishnu.dev.roy@gmail.com>
2022-06-30 23:11:20 +0530

    call,apply,bind

---
+++ programmingTechniques/callApplyBind.js
@@ -0,0 +1,26 @@
+let userDetails = {
+    name: "jishnu",
+    age: "25",
+    designation: "Software Engineer",
+    // printDetails: function() {
+    //     console.log(this);
+    // }
+}
+ let printDetails = function(state) {
+        console.log(this.name + " " + state);
+    }
+
+let userDetails2 = {
+    name: "Biswa",
+    age: "25",
+    designation: "Software Engineer",
+}
+// userDetails.printDetails();
+// function borrowing
+// userDetails.printDetails.call(userDetails2);
+printDetails.call(userDetails, "Malda");
+
+printDetails.apply(userDetails, ["Malda"]);
+
+let nFun = printDetails.bind(userDetails, "bind");
+nFun();
